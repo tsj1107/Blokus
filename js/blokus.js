a6a2eb19c170a2fbdfa9422e11fabeaf8e2af40e
夏奈 <shuaiji.tsj@alibaba-inc.com>
2014-11-19 12:04:54 +0800

    解决标记取消标记的问题

--- js/blokus.js
+++ js/blokus.js
@@ -1,26 +1,22 @@
 var pieceData = {
-  'a': {
-    coords: [
-      [0, 0]
-    ],
-    size: [1, 1]
-  },
-  'b': {
-    coords: [
-      [0, 0],
-      [0, 1]
-    ],
-    size: [1, 2]
-  },
-  'c': {
-    coords: [
-      [0, 0],
-      [0, 1],
-      [1, 1]
-    ],
-    size: [2, 2]
-  }
+  'a': [1],
+  'b': [1, 1],
+  'c': [
+    [1, 1],
+    [0, 1]
+  ],
+  'd': [1, 1, 1],
+  'e': [
+    [1, 1],
+    [1, 1]
+  ],
+  'f': [
+    [0, 1, 0],
+    [1, 1, 1]
+  ]
 };
+var boardMatrix = [];
+
 var BLOKUS = {
   pixSize: 20,
   boardSize: 14
@@ -34,30 +30,57 @@ var pieceLayer = new Kinetic.Layer({
     x: 1,
     y: 1
   });
+
+var stage = new Kinetic.Stage({
+  container: 'container',
+  width: 800,
+  height: 800
+});
+
 var num = 0;
 function piece(id, item) {
   var group = new Kinetic.Group({
     x: num * 100,
     y: 0,
-    width: item.size[0] * BLOKUS.pixSize,
-    height: item.size[1] * BLOKUS.pixSize,
+    width: item.length * BLOKUS.pixSize,
+    height: item[0].length ? (item[0].length * BLOKUS.pixSize) : BLOKUS.pixSize,
     draggable: true
   });
-  for (var i = 0; i < item.coords.length; i++) {
-    group.add(new Kinetic.Rect({
-      x: BLOKUS.pixSize * item.coords[i][0],
-      y: BLOKUS.pixSize * item.coords[i][1],
-      width: BLOKUS.pixSize,
-      height: BLOKUS.pixSize,
-      fill: '#00D2FF',
-      stroke: 'black',
-      strokeWidth: 1
-    }))
+
+  for (var i = 0; i < item.length; i++) {//纵向
+    if (item[0].length) {
+      for (var j = 0; j < item[0].length; j++) {//横向
+        if (item[i][j] != 0) {
+          group.add(new Kinetic.Rect({
+            x: BLOKUS.pixSize * j,
+            y: BLOKUS.pixSize * i,
+            width: BLOKUS.pixSize,
+            height: BLOKUS.pixSize,
+            fill: '#00D2FF',
+            stroke: 'black',
+            strokeWidth: 1
+          }))
+        }
+      }
+    } else {
+      if (item[i] != 0) {
+        group.add(new Kinetic.Rect({
+          x: BLOKUS.pixSize * i,
+          y: 0,
+          width: BLOKUS.pixSize,
+          height: BLOKUS.pixSize,
+          fill: '#00D2FF',
+          stroke: 'black',
+          strokeWidth: 1
+        }))
+      }
+    }
   }
 
   this.id = id;
-  this.coords = item.coords;
+  this.metrix = item;
   this.shape = group;
+  this.color = 1;
   num++;
 }
 
@@ -96,50 +119,72 @@ function board() {
 }
 
 function init() {
-  var stage = new Kinetic.Stage({
-    container: 'container',
-    width: 800,
-    height: 800
-  });
+
+  var tempArr;
+  for (var j = 0; j < BLOKUS.boardSize; j++) {
+    tempArr = [];
+    for (var i = 0; i < BLOKUS.boardSize; i++) {
+      tempArr.push(0)
+    }
+    boardMatrix.push(tempArr)
+  }
 
   boardLayer.add(board());
   for (item in pieceData) {
-    (function(){
+    (function () {
       var obj = new piece(item, pieceData[item]),
         shape = obj.shape;
 
+//  TODO group cache 报错
 //      shape.cache()
 //      shape.filter([
 //        Kinetic.Filters.Blur,
 //        Kinetic.Filters.Brighten
 //      ])
 
-      shape.on('dragstart', function(){
+      shape.on('dragstart', function () {
         this.moveToTop();
         pieceLayer.draw();
+        var gridPos = {
+          x: Math.round(shape.getX() / BLOKUS.pixSize),
+          y: Math.round(shape.getY() / BLOKUS.pixSize)
+        }
+        if(shape.isStick){
+          revokeFlag(obj, gridPos);
+          shape.isStick = false;
+        }
       })
 
-      shape.on('dragmove', function(){
+      shape.on('dragmove', function () {
         document.body.style.cursor = 'pointer';
       })
 
-      shape.on('dragend', function(){
-        var pos = {
-          x: Math.round(shape.getX()/BLOKUS.pixSize)*BLOKUS.pixSize,
-          y: Math.round(shape.getY()/BLOKUS.pixSize)*BLOKUS.pixSize
-        };
-        shape.setPosition(pos);
+      shape.on('dragend', function () {
+        shape.isStick = false;
+        var gridPos = {
+          x: Math.round(shape.getX() / BLOKUS.pixSize),
+          y: Math.round(shape.getY() / BLOKUS.pixSize)
+        }
+        if(isValidate(obj.metrix, gridPos)){
+          var pos = {
+            x: gridPos.x * BLOKUS.pixSize,
+            y: gridPos.y * BLOKUS.pixSize
+          };
+          shape.setPosition(pos);
+          shape.isStick = true;
+          markFlag(obj, gridPos);
+        }
         pieceLayer.draw();
       })
 
-      shape.on('mouseover', function(){
+      shape.on('mouseover', function () {
         shape.blurRadius(2);
         shape.brightness(0.5);
         pieceLayer.draw();
         document.body.style.cursor = 'pointer';
       })
 
-      shape.on('mouseout', function(){
+      shape.on('mouseout', function () {
         shape.blurRadius(0);
         shape.brightness(0);
         pieceLayer.draw();
@@ -147,6 +192,7 @@ function init() {
       })
 
       pieceLayer.add(shape)
+
     })()
   }
 
@@ -154,61 +200,81 @@ function init() {
   stage.add(pieceLayer);
 }
 
-function DataBinder( object_id ) {
-  // Create a simple PubSub object
-  var pubSub = {
-      callbacks: {},
-
-      on: function( msg, callback ) {
-        this.callbacks[ msg ] = this.callbacks[ msg ] || [];
-        this.callbacks[ msg ].push( callback );
-      },
-
-      publish: function( msg ) {
-        this.callbacks[ msg ] = this.callbacks[ msg ] || []
-        for ( var i = 0, len = this.callbacks[ msg ].length; i < len; i++ ) {
-          this.callbacks[ msg ][ i ].apply( this, arguments );
-        }
-      }
-    },
-
-    data_attr = "data-bind-" + object_id,
-    message = object_id + ":change",
-
-    changeHandler = function( evt ) {
-      var target = evt.target || evt.srcElement, // IE8 compatibility
-        prop_name = target.getAttribute( data_attr );
-
-      if ( prop_name && prop_name !== "" ) {
-        pubSub.publish( message, prop_name, target.value );
-      }
-    };
-
-  // Listen to change events and proxy to PubSub
-  if ( document.addEventListener ) {
-    document.addEventListener( "change", changeHandler, false );
-  } else {
-    // IE8 uses attachEvent instead of addEventListener
-    document.attachEvent( "onchange", changeHandler );
-  }
-
-  // PubSub propagates changes to all bound elements
-  pubSub.on( message, function( evt, prop_name, new_val ) {
-    var elements = document.querySelectorAll("[" + data_attr + "=" + prop_name + "]"),
-      tag_name;
-
-    for ( var i = 0, len = elements.length; i < len; i++ ) {
-      tag_name = elements[ i ].tagName.toLowerCase();
-
-      if ( tag_name === "input" || tag_name === "textarea" || tag_name === "select" ) {
-        elements[ i ].value = new_val;
-      } else {
-        elements[ i ].innerHTML = new_val;
+function isValidate(metrix, gridPos){
+  if(typeof metrix[0] === 'number'){
+    for(var i=0; i<metrix.length; i++){
+      if(metrix[i]&&boardMatrix[gridPos.x+i][gridPos.y]){
+        return false;
       }
     }
-  });
+  }else{
+    var tempArr;
+    for(var i=0; i<metrix.length; i++){
+      tempArr = metrix[i];
+      for(var j=0; j<tempArr.length; j++){
+        if(metrix[i][j]&&boardMatrix[gridPos.x+j][gridPos.y+i]){
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+}
 
-  return pubSub;
+function revokeFlag(piece, gridPos){
+  var metrix = piece.metrix;
+  if(typeof metrix[0] === 'number'){
+    for(var i=0; i<metrix.length; i++){
+      if(metrix[i]){
+        boardMatrix[gridPos.x+i][gridPos.y] = 0;
+      }
+    }
+  }else{
+    var tempArr;
+    for(var i=0; i<metrix.length; i++){
+      tempArr = metrix[i];
+      for(var j=0; j<tempArr.length; j++){
+        if(metrix[i][j]){
+          boardMatrix[gridPos.x+j][gridPos.y+i] = 0;
+        }
+      }
+    }
+  }
+  print()
+}
+
+function markFlag(piece, gridPos){
+  var metrix = piece.metrix;
+  if(typeof metrix[0] === 'number'){
+    for(var i=0; i<metrix.length; i++){
+      if(metrix[i]) {
+        boardMatrix[gridPos.x + i][gridPos.y] = piece.color;
+      }
+    }
+  }else{
+    var tempArr;
+    for(var i=0; i<metrix.length; i++){
+      tempArr = metrix[i];
+      for(var j=0; j<tempArr.length; j++){
+        if(metrix[i][j]){
+          boardMatrix[gridPos.x+j][gridPos.y+i] = piece.color;
+        }
+      }
+    }
+  }
+  print()
+}
+
+function print(){
+  var elm = document.getElementById('console'), html = '';
+  elm.innerHTML = html;
+  for(var i=0; i<boardMatrix.length; i++){
+    for(var j=0; j<boardMatrix[i].length; j++){
+      html += boardMatrix[j][i]+' ';
+    }
+    html += '<br>'
+  }
+  elm.innerHTML = html;
 }
 
 init()
